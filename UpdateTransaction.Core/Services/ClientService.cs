b5a15f154d568a9cb4154004e9590a4e582e7784
Oladeji Mujib <oladejimujib@gmail.com>
2023-06-03 19:05:20 +0100

    update

--- UpdateTransaction.Core/Services/ClientService.cs
+++ UpdateTransaction.Core/Services/ClientService.cs
@@ -1,29 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using UpdateTransaction.Core.DTO;
+﻿using UpdateTransaction.Core.DTO;
 using UpdateTransaction.Core.Interfaces.RepositoryInterface;
 using UpdateTransaction.Core.Interfaces.ServiceInterface;
 using UpdateTransaction.Core.Model;
-using UpdateTransaction.Core.Repositories;
 
 namespace UpdateTransaction.Core.Services
 {
     public class ClientService:IClientService
     {
         private readonly IClientRepository _clientRepository;
-        private readonly IWalletRepository _walletRepository;
-        private readonly ILogger<ClientService> _logger;
+    
 
-        public ClientService( IClientRepository clientRepository, IWalletRepository walletRepository, ILogger<ClientService> logger)
+        public ClientService( IClientRepository clientRepository)
         {
             _clientRepository = clientRepository;
-            _walletRepository = walletRepository;
-            _logger = logger;
+           
         }
 
         public async Task<BaseResponseModel<ClientResponseModel>> CreateClient(CreateClientRequestModel model)
